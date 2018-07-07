de983a8e0044bb5982acf12bd99b1d9c20bfec0a
unknown <ehurst01@gmail.com>
2018-07-06 22:29:01 -0700

    Protected Route Stub working

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Users from "./pages/Users/Users";
 import NoMatch from "./pages/NoMatch";  
@@ -12,17 +12,27 @@ import BrowseSubscriptions from './pages/Subscriptions/BrowseSubscriptions'
 import "./App.css";
 import Secret from './components/Secret'
 import { PromiseProvider } from "mongoose";
+import Auth from './Auth'
+
+
+
+const PrivateRoute = ({ component: Component, ...rest}) => (
+  <Route {...rest} render={(props) => (
+    props.auth.isAuthenticated === true
+      ? <Component {...props} />
+      : <Redirect to='/login' />
+  )} />
+)
+
 
 
 class App extends Component {
-
   render() {
     return(
-
-
   <Router>
     <div>
       <Nav {...this.props}/>
+      <div>{this.props.name}</div>
       <Wrapper>
       <Switch>
         <Route exact path="/" component={Landing} data />
@@ -31,7 +41,7 @@ class App extends Component {
         <Route exact path="/users/:id" component={Users} />
         <Route exact path='/subscription/new' component={NewSubscription} />
         <Route exact path='/subscription' component={BrowseSubscriptions} />
-        <Route exact path='/secret' component={Secret} />
+        <PrivateRoute {...this.props} exact path='/secret' component={Secret} />
         <Route component={NoMatch} />
       </Switch>
       </ Wrapper>
--- client/src/Auth.js
+++ client/src/Auth.js
@@ -1,4 +1,9 @@
+/* eslint no-restricted-globals: 0 */
+
 import auth0 from 'auth0-js';
+
+const LOGIN_SUCCESS_PAGE = '/secret'
+const LOGIN_FAILURE_PAGE = '/subscription'
 
 export default class Auth {
     auth0 = new auth0.WebAuth({
@@ -17,5 +22,30 @@ export default class Auth {
     login(){
         this.auth0.authorize()
     }
-}
 
+    handleAuthentication(){
+        this.auth0.parseHash((err, authResults)=>{
+            if(err){
+                location.pathname = LOGIN_FAILURE_PAGE;
+                console.log(err);
+            }
+            
+            else if(authResults && authResults.accessToken && authResults.idToken)
+            {
+                let expiresAt=JSON.stringify((authResults.expiresIn)*1000 + new Date().tetTime());
+                localStorage.setItem("access_token", authResults.accessToken);
+                localStorage.setItem("id_token", authResults.idToken);
+                localStorage.setItem("expires_at", expiresAt);
+                location.hash = "";
+                location.pathname = LOGIN_SUCCESS_PAGE;
+            }
+            
+        })
+    }
+
+    isAuthenticated(){
+        let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+        return new Date().getTime() < expiresAt;
+    }
+
+}
