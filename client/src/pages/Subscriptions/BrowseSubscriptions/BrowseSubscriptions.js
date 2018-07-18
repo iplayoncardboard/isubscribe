4197c8fe04f22fdfe3d051a912cbff60ed20ee93
unknown <ehurst01@gmail.com>
2018-07-17 20:55:34 -0700

    updated the browse subscriptions, cleaned up state and fixed passing _id

--- client/src/pages/Subscriptions/BrowseSubscriptions/BrowseSubscriptions.js
+++ client/src/pages/Subscriptions/BrowseSubscriptions/BrowseSubscriptions.js
@@ -10,15 +10,15 @@ import Popup from "reactjs-popup";
 
 class BrowseSubscriptions extends Component {
     state = {   
-        subscriptionName: [], 
-        price: [],
-        description: "",
-        category: "",
-        iconURL: "",
-        url: "",
-        price: {},
+        subscriptionObject: [],
         categories:[],
+        addSubscription: this.addSubscription
       };
+
+
+addSubscription = () =>{
+    API.
+}
 
 componentDidMount() {
      this.loadSubscriptions();
@@ -30,15 +30,12 @@ componentDidMount() {
 loadSubscriptions = () => {
      API.getSubscriptions() 
     // API.getMusic()   
-        .then(res =>
+        .then(res => {
+            // console.log(res.data[0]._id);
             this.setState({
-            subscriptionName: res.data, 
-            price: res.data,
-            description: "",
-            category: "",
-            iconURL: "",
-            url: "",
+            subscriptionObject: res.data
          })
+        }
     )
     .catch(err => console.log(err));
 
@@ -48,45 +45,25 @@ loadCatSubscriptions = catFilter => {
     if (catFilter === "Music Streaming") {
         API.getMusic().then(res =>
             this.setState({
-                subscriptionName: res.data, 
-                price: res.data,
-                description: "",
-                category: "",
-                iconURL: "",
-                url: "",
+                subscriptionObject: res.data
             })).catch(err => console.log(err));
     }
     if (catFilter === "Food") {
         API.getFood().then(res =>
             this.setState({
-                subscriptionName: res.data, 
-                price: res.data,
-                description: "",
-                category: "",
-                iconURL: "",
-                url: "",
+                subscriptionObject: res.data
             })).catch(err => console.log(err));
     }
     if (catFilter === "Fashion") {
         API.getFashion().then(res =>
             this.setState({
-                subscriptionName: res.data, 
-                price: res.data,
-                description: "",
-                category: "",
-                iconURL: "",
-                url: "",
+                subscriptionObject: res.data
             })).catch(err => console.log(err));
     }
     if (catFilter === "Video Streaming Service") {
         API.getVideo().then(res =>
             this.setState({
-                subscriptionName: res.data, 
-                price: res.data,
-                description: "",
-                category: "",
-                iconURL: "",
-                url: "",
+                subscriptionObject: res.data
             })).catch(err => console.log(err));
     }
 };
@@ -108,6 +85,12 @@ handleInputChange = event => {
           });
     };
 
+showState = (event) => {
+    event.preventDefault();
+    console.log(`State ${JSON.stringify(this.state.subscriptionObject)}`);
+    console.log(`ID 0: ${this.state.subscriptionObject[0]._id}`);
+}
+
 render(){
     return(
         <div>
@@ -121,7 +104,7 @@ render(){
                  
                 {/* <a className="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home" onClick={() => this.loadSubscriptions()}>All Subscriptions</a> */}
                     {this.state.categories.map(categoryName =>( 
-                        <CatNav key={categoryName._id} categoryName={categoryName.name} loadCatSubscriptions={this.loadCatSubscriptions}/> 
+                        <CatNav id={categoryName._id} categoryName={categoryName.name} loadCatSubscriptions={this.loadCatSubscriptions}/> 
                     ))} 
                 </div>    
             </div>
@@ -130,24 +113,21 @@ render(){
     <div className="tab-content" id="nav-tabContent">
       <div className="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
       
-        {this.state.subscriptionName.map(subscriptions => (
+        {this.state.subscriptionObject.map((subscription) => (
             <BrowseCard
-                key={subscriptions._id}
-                name={subscriptions.subscriptionName}
-                price={subscriptions.price}
-                category={subscriptions.category}
-                description={subscriptions.description}
-                iconURL={subscriptions.iconURL}
-                url={subscriptions.url}
-                price={subscriptions.price}
+                id={subscription._id}
+                name={subscription.subscriptionName}
+                price={subscription.price}
+                category={subscription.category}
+                description={subscription.description}
+                iconURL={subscription.iconURL}
+                url={subscription.url}
             />
-       
-            
             ))}
-
         </div>
     </div>
   </div>    
+  <button onClick={this.showState}> Show State </button>
 </div>
 
 
